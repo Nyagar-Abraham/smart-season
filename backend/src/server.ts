1d3b7dcdb20550de310ee3abff46e272963d7c82
Nyagar-Abraham <abrahamowino129@gmail.com>
2026-04-20 21:47:26 +0300

    fir cors issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -27,7 +27,7 @@ const API_BASE_URL = process.env.API_BASE_URL || '/api/v1';
 const COOKIE_PARSER_SECRET = process.env.COOKIE_PARSER_SECRET;
 
 app.use(cors({
-  origin: 'https://www.hxrrvpsxtz.xyz',
+  origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : 'http://localhost:5173',
   credentials: true,
 }));
 
